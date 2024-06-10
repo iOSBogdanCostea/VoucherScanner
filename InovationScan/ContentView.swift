dd1db9d879f458e04db32e7bc237e42f2184f730
Bogdan Costea <bogdan.costea@x2mobile.net>
2024-06-10 16:20:49 +0300

    Initial Commit

---
+++ InovationScan/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InovationScan
+//
+//  Created by Bogdan Costea on 10.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InovationScan/InovationScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  InovationScanApp.swift
+//  InovationScan
+//
+//  Created by Bogdan Costea on 10.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct InovationScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
